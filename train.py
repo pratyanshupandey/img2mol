c99a0ec180cf42e1b28e5700624fe6134bbe85c3
pratyanshupandey <pratyan2001@gmail.com>
2022-05-04 07:22:02 +0530

    loading from checkpoints

--- train.py
+++ train.py
@@ -3,22 +3,31 @@ from torch import nn
 from model import IMG2MOL
 from utils import Trainer
 from dataset import MoleculeData
+from early_stopping import EarlyStopping
+import argparse
+
+parser = argparse.ArgumentParser(description='Get configurations to train')
+parser.add_argument('--cpu_cores', default=15, type=int)
+parser.add_argument('--model', default="", type=str)
+CONFIG = parser.parse_args()
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Using device: ", device)
 
 
-train_path = "data/data/train/"
-val_path = "data/data/val/"
-test_path = "data/data/test/"
+train_path = "data/train/"
+val_path = "data/val/"
+test_path = "data/test/"
 checkpoint_dir = "checkpoints/"
 
 
 #Hyperparameters
-batch_size = 32
-learning_rate = 0.00001
+batch_size = 128
+learning_rate = 1e-6#0.00005
 epochs = 300
-cpu_cores = 20
+cpu_cores = CONFIG.cpu_cores
+print(f"Using {cpu_cores} CPU cores")
 
 # Getting data loaders
 train_dataset = MoleculeData(train_path)
@@ -33,15 +42,16 @@ val_loader = torch.utils.data.DataLoader(val_dataset,
                                             shuffle=False, 
                                             num_workers=cpu_cores)
 
-test_dataset = MoleculeData(test_path)
-test_loader = torch.utils.data.DataLoader(test_dataset, 
-                                            batch_size=batch_size,
-                                            shuffle=False, 
-                                            num_workers=cpu_cores)
-
-
 # Getting the model
-model = IMG2MOL().to(device)
+model = IMG2MOL()
+
+if CONFIG.model == "":
+    print("Training new model")
+else:
+    print("Using model from", CONFIG.model)
+    model.load_state_dict(torch.load(CONFIG.model))
+
+model = model.to(device)
 
 
 # Optimizer and Criterion
@@ -49,12 +59,13 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, 
                                                         mode='min', 
-                                                        factor=0.7, 
-                                                        patience=10, 
+                                                        factor=0.5, 
+                                                        patience=3, 
                                                         verbose=True)  
 
 
-
+# Early Stopping
+early_stopping = EarlyStopping(patience=5)
 
 # Train the model
 trainer = Trainer(model_name="Img2Mol",
@@ -66,6 +77,7 @@ trainer = Trainer(model_name="Img2Mol",
                     train_loader=train_loader,
                     val_loader=val_loader,
                     device=device,
-                    checkpoint_dir=checkpoint_dir)
+                    checkpoint_dir=checkpoint_dir,
+                    early_stopping=early_stopping)
 
 trainer.train()
--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-
 import torch
-
+# from inference import Inference
 
 class Trainer:
-    def __init__(self, model_name, model, criterion, optimizer, scheduler, epochs, train_loader, val_loader, device, checkpoint_dir):
+    def __init__(self, model_name, model, criterion, optimizer, scheduler, epochs, train_loader, val_loader, device, checkpoint_dir, early_stopping):
         self.model_name = model_name
         self.model = model
         self.criterion = criterion
@@ -14,23 +13,23 @@ class Trainer:
         self.val_loader = val_loader
         self.device = device
         self.checkpoint_dir = checkpoint_dir
+        self.early_stopping = early_stopping
         if self.checkpoint_dir[-1] != '/':
             self.checkpoint_dir += '/'
+        
+        # self.inference = Inference(model=self.model,
+        #                             data_loader=self.val_loader,
+        #                             device=self.device)
 
     def train(self):
         print(f"Started Training of model {self.model_name}")
         self.model = self.model.to(self.device)
 
-        # Early stopping parameters
-        min_loss = 1e5
-        patience = 50
-        current_count = 0
-
         for epoch in range(self.epochs):
             running_loss = 0.0
 
             # Train for an epoch
-            for i, (images, embeddings) in enumerate(self.train_loader):
+            for i, (smiles, images, embeddings) in enumerate(self.train_loader):
                 images = images.to(self.device)
                 embeddings = embeddings.to(self.device)
 
@@ -47,30 +46,30 @@ class Trainer:
                 running_loss += loss.data
                 if i % 50 == 49:
                     print('[%d, %d] loss: %.5f' %
-                          (epoch + 1, i + 1, running_loss / 50))
+                          (epoch + 1, i + 1, running_loss / 50), flush=True)
                     running_loss = 0.0
 
             # Calculate validation loss
             val_loss = self.calculate_val_loss()
+            print('Epoch: %d Validation loss: %.5f' % (epoch + 1, val_loss), flush=True)
 
             # Take a scheduler step
             self.scheduler.step(val_loss)
 
-            # Check for early stopping
-            print('Epoch: %d Validation loss: %.5f' %
-                  (epoch + 1, val_loss))
-            if val_loss < min_loss:
-                current_count = 0
-                min_loss = val_loss
-            else:
-                current_count += 1
-                if current_count >= patience:
-                    print("Early Stopping the training")
-                    break
+            # Take a early stopping step
+            self.early_stopping.step(val_loss)
             
-            # save a model every 50 epochs
-            if epoch % 50 == 49:
-                self.save_model(f"checkpoint_{epoch // 50}")
+            # save a model every 5 epochs and also calculate the accuracy
+            if epoch % 5 == 4:
+                self.save_model(f"checkpoint_{epoch}")
+            # _, accuracy = self.inference.calculate_loss_accuracy()
+            # print('Epoch: %d Validation Accuracy: %.5f' %
+            #   (epoch + 1, accuracy))
+            
+            # Check early stopping to finish training
+            if self.early_stopping.stop_training:
+                print("Early Stopping the training")
+                break
 
         print('Finished Training')
         self.save_model("final")
@@ -81,7 +80,7 @@ class Trainer:
 
         # Test validation data
         with torch.no_grad():
-            for i, (images, embeddings) in enumerate(self.val_loader):
+            for i, (smiles, images, embeddings) in enumerate(self.val_loader):
                 images = images.to(self.device)
                 embeddings = embeddings.to(self.device)
 
